348f7700488c0142f113ab84697e488059ed0781
Oğulcan Özen <ogulcan.ozen@std.yeditepe.edu.tr>
2023-11-11 16:10:01 +0300

    Add the Game

---
+++ Assets/Driver.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 250f;
+    [SerializeField] float moveSpeed = 0.3f;
+    [SerializeField] float slowSpeed = 0.17f;
+    [SerializeField] float boostSpeed = 0.45f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
+    }
+}
